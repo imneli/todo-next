9062a027d9ea49b94e30b0d17df63b58ad0a6137
imneli <contatodoneli@gmail.com>
2024-10-16 18:16:41 -0300

    miliseconds

--- src/pages/task/[id].tsx
+++ src/pages/task/[id].tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import styles from './styles.module.css'
 import { GetServerSideProps } from 'next'
+import { db } from '@/services/firebaseConnection'
+import { doc, collection, query, where, getDoc } from 'firebase/firestore'
 
 export default function Task() {
     return(
@@ -21,7 +23,37 @@ export default function Task() {
 export const getServerSideProps: GetServerSideProps = async({ params }) => {
 
     const id = params?.id as string
-    console.log(id)
+    const docRef = doc(db, "tarefas", id)
+
+    const snapshot = await getDoc(docRef)
+
+    if (snapshot.data() === undefined) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
+            }
+        }
+    }
+
+    if (!snapshot.data()?.public) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
+            }
+        } 
+    }
+
+    const miliseconds = snapshot.data()?.created?.seconds * 1000
+
+    const task = {
+        tarefa: snapshot.data()?.tarefa,
+        public: snapshot.data()?.public,
+        created: new Date(miliseconds).toLocaleDateString(),
+        user: snapshot.data()?.user,
+        taskId: id,
+    }
 
     return {
         props: {}
