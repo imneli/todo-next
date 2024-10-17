728aba568836a64c94c701712afe5ac0f923b1ea
imneli <contatodoneli@gmail.com>
2024-10-17 12:15:07 -0300

    header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,44 +1,58 @@
 import styles from "./styles.module.css";
 import Link from "next/link";
-
 import { useSession, signIn, signOut } from 'next-auth/react'
+import { useState } from 'react';
 
 export function Header() {
+  const { data: session, status } = useSession();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const { data: session, status } = useSession()
-
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
     <header className={styles.header}>
       <section className={styles.content}>
-        <nav className={styles.nav}>
+        <div className={styles.logoContainer}>
           <Link href="/">
             <h1 className={styles.logo}>
               My<span>Tasks</span>
             </h1>
           </Link>
-        { session?.user && (
-          <Link href="/dashboard" className={styles.link}>
-            Meu Painel
-          </Link>
-        ) }
-        </nav>
-
-        { status === "loading" ? (
-          <></>
-        ) : session ? (
-          <>
-          <div className={styles.boxButtons}>
-          <button className={styles.loginButton} onClick={ () => signOut()}>
-            Olá {session?.user?.name}
+          <button 
+            className={`${styles.burgerButton} ${isMenuOpen ? styles.active : ''}`}
+            onClick={toggleMenu}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
           </button>
         </div>
-        </>
-        ) : (
-          <button className={styles.loginButton} onClick={ () => signIn("google")}>
-            Acessar
-          </button>
-        )}
+
+        <div className={`${styles.mobileMenu} ${isMenuOpen ? styles.active : ''}`}>
+          <nav className={styles.nav}>
+            {session?.user && (
+              <Link href="/dashboard" className={styles.link}>
+                Meu Painel
+              </Link>
+            )}
+          </nav>
+
+          {status === "loading" ? (
+            <></>
+          ) : session ? (
+            <div className={styles.boxButtons}>
+              <button className={styles.loginButton} onClick={() => signOut()}>
+                Olá {session?.user?.name}
+              </button>
+            </div>
+          ) : (
+            <button className={styles.loginButton} onClick={() => signIn("google")}>
+              Acessar
+            </button>
+          )}
+        </div>
       </section>
     </header>
   );
