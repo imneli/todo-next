72d89dde1533f88df606033deecfb4465ecf94e6
imneli <contatodoneli@gmail.com>
2024-10-17 12:23:04 -0300

    path

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import localFont from "next/font/local";
-import styles from "@/styles/home.module.css";
+import styles from "../styles/home.module.css";
 import HeroImage from '../../public/assets/hero.png';
 import { GetStaticProps } from "next";
 import { collection, getDocs } from "firebase/firestore";
